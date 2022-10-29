6c5333a716f714db204413d1bbec37af3eead123
Lucas Cerqueira <huryu@hotmail.com>
2022-10-29 00:22:06 -0200

    testing connection

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
-use std::net::{TcpListener};
+use std::fs;
+use std::io::prelude::*;
+use std::net::{TcpListener, TcpStream};
 
 const SERVER_IP: &str = "127.0.0.1";
 const SERVER_PORT: &str = "8080";
+
+fn handle_connection(stream: TcpStream) {
+    // TODO
+}
 
 fn main() {
     let listener = TcpListener::bind(format!("{}:{}", SERVER_IP, SERVER_PORT)).unwrap();
     println!("Listening on ip: {}, port: {}", SERVER_IP, SERVER_PORT);
 
     for stream in listener.incoming() {
-        let _stream = stream.unwrap();
+        let stream = stream.unwrap();
+        handle_connection(stream);
+
+        println!("Connected!");
     }
 }
